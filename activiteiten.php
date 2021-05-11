ee074af0dd3ee8b597a26a0e2b1e6e0bf273a0f2
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-05-11 16:45:51 +0200

    11-05-2021

--- activiteiten.php
+++ activiteiten.php
@@ -33,7 +33,7 @@
         <div class="page-left">
             <p class="titels">Activiteiten:</p><hr></br>
             <p class="page-text">
-                Wij wandelen in het weekend Op zaterdag start de wandeling om 11:00 en zondag om 11:00. 
+                Wij wandelen in het weekend Op zaterdag start de wandeling om 11:00. 
                 Wij wandelen eenmaal per dag. Door de week wandelen we op aanvraag. 
                 We starten op de Lage Baan en rijden daarna naar het bos om te wandelen. 
                 Bij terugkomst mag u de alpaca’s nog hun welverdiende wortel geven en kunt u nagenieten met een kopje koffie of thee. 
