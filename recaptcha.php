216ab773850ef7c1a8592955dae61c4b7b3ad6fc
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-05-13 16:01:18 +0200

    reCaptcha

--- recaptcha.php
+++ recaptcha.php
@@ -8,7 +8,7 @@
     <form action="" method="POST">
         <input type="text" name="text">
         <input type="email" name="email">
-        <div class="g-recaptcha" style="margin-top: 10px;" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div>
+        <div class="g-recaptcha" style="margin-top: 10px;" data-sitekey="6Lc7LtMaAAAAAIYagSxe2mo4XLwP95T1M4lq4evM"></div>
         <input type="submit">
     </form>
 </body>
