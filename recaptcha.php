fb6dc429d3794181b7b210190b28e263ebb44637
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-05-13 16:08:09 +0200

    reCaptcha

--- recaptcha.php
+++ recaptcha.php
@@ -2,13 +2,21 @@
 <html lang="en">
 <head>
     <title>Document</title>
-    <script src='https://www.google.com/recaptcha/api.js'></script> 
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script>
+        function onSubmit(token) {
+          document.getElementById("demo-form").submit();
+        }
+    </script>
 </head>
 <body>
     <form action="" method="POST">
         <input type="text" name="text">
         <input type="email" name="email">
-        <div class="g-recaptcha" style="margin-top: 10px;" data-sitekey="6Lc7LtMaAAAAAIYagSxe2mo4XLwP95T1M4lq4evM"></div>
+            <button class="g-recaptcha" 
+            data-sitekey="reCAPTCHA_site_key" 
+            data-callback='onSubmit' 
+            data-action='submit'>Submit</button>
         <input type="submit">
     </form>
 </body>
