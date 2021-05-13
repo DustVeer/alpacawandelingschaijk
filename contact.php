9e4bb541bf858e9aba76cd513e78b4708b508aaf
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-05-13 15:54:07 +0200

    reCaptcha

--- contact.php
+++ contact.php
@@ -115,7 +115,7 @@ else { $succes = ""; }
                     <textarea class="form-questions reservering" name="remark"></textarea>
                     
 
-                    <div class="g-recaptcha" style="margin-top: 10px;" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div>
+                    <!-- <div class="g-recaptcha" style="margin-top: 10px;" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div> -->
 
                     <!-- SUBMIT -->
                     <div class="form-submit">
