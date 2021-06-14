d7f846d44ddb23bcd613406d03b47d3ac2b2bcd1
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-06-14 10:38:12 +0200

    14-06-2021

--- classes/pdo.php
+++ classes/pdo.php
@@ -1,16 +1,9 @@
 <?php 
-if (true)
-{
-    define("dbname", "ernstandereogen_alpacawandelingschaijk");
-    define("dbuser", "ernstandereogen_ernstandereogen");
-    define("dbpass", "GQ-jWzm37gPdx");
-}
-else
-{
-    define("dbname", "ernstandereogen_alpacawandelingschaijk");
-    define("dbuser", "root");
-    define("dbpass", "");
-}
+
+
+define("dbname", "ernstandereogen_alpacawandelingschaijk");
+define("dbuser", "ernstandereogen_ernstandereogen");
+define("dbpass", "GQ-jWzm37gPdx");
 
 
 $pdo = new PDO("mysql:host=localhost;dbname=" . dbname, dbuser, dbpass);
